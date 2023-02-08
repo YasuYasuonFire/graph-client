19f799c8b7d3986ef3798f7afbc5879410dd910a
yasuyasu <faithlessboy007@yahoo.co.jp>
2023-02-08 12:22:55 +0900

    Your lucky token initial

--- examples/react-query/src/App.tsx
+++ examples/react-query/src/App.tsx
@@ -1,39 +1,77 @@
-import logo from './logo.svg'
-import './App.css'
-import { getBuiltGraphSDK } from '../.graphclient'
-import { useQuery } from 'react-query'
-
-const sdk = getBuiltGraphSDK()
+import React, { useEffect } from 'react'
+import gql from 'graphql-tag'
+// ...
+// we import types and typed-graphql document from the generated code (`..graphclient/`)
+import { execute } from '../.graphclient'
 
 function App() {
-  const result = useQuery('ExampleQuery', () => sdk.ExampleQuery())
+  const [data, setData] = React.useState('')
 
-  const { data, isLoading, error, refetch } = result
+  // const myQuery = gql`
+  //   query pairs {
+  //     pair(id: "0x00004ee988665cdda9a1080d5792cecd16dc1220") {
+  //       id
+  //       token0 {
+  //         id
+  //         symbol
+  //         name
+  //       }
+  //       token1 {
+  //         id
+  //         symbol
+  //         name
+  //       }
+  //     }
+  //   }
+  // `
+
+  //generate random number function
+  const getRandomInt = (max: number) => {
+    console.log('getRandomInt')
+    return Math.floor(Math.random() * max)
+  }
+
+  const myQuery = gql`
+    query tokens($skipNum: Int!) {
+      tokens(first: 1, skip: $skipNum) {
+        id
+        symbol
+        name
+      }
+    }
+  `
+
+  useEffect(() => {}, [])
+
+  const executeQuery = () => {
+    //set variable for skip
+    let variables = {
+      skipNum: getRandomInt(1000),
+    }
+    execute(myQuery, variables).then((result) => {
+      setData(result?.data)
+    })
+    console.log('setData')
+    // console.log(data)
+    // console.log(Object.keys(data))
+    // console.log(data["tokens"][0].id)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Graph Client with React Query Example</p>
-        <p>
-          <button type="button" onClick={() => refetch()} disabled={isLoading}>
-            Re Execute Query
-          </button>
-        </p>
-        <p>{isLoading ? 'Loading...' : 'You can find the result below...'}</p>
+        <p>Your Lucky Token</p>
+        <button onClick={executeQuery}>Go!</button>
         <fieldset>
           {data && (
-            <form>
-              <label>Data</label>
-              <br />
-              <textarea value={JSON.stringify(data, null, 2)} readOnly rows={25} />
-            </form>
-          )}
-          {error && (
-            <form>
-              <label>Error</label>
-              <br />
-              <textarea value={JSON.stringify(error, null, 2)} readOnly rows={25} />
-            </form>
+            <>
+              <p>contract address: {data['tokens'][0].id}</p>
+              <p>symbol: {data['tokens'][0].symbol}</p>
+              <p>name: {data['tokens'][0].symbol}</p>
+              <a href={'https://app.uniswap.org/#/tokens/ethereum/' + data['tokens'][0].id} target="_blank">
+                Check on Uniswap!
+              </a>
+            </>
           )}
         </fieldset>
       </header>
